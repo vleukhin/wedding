01b6f9024d478263f1d24b6a326b39ddee4a4770
vleukhin <norad-2013@mail.ru>
2018-02-15 14:17:14 +0700

    базовая модель и модель приглашения

---
+++ app/Models/Invite.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by Viktor Leukhin.
+ * Tel: +7-926-797-5419
+ * E-mail: vleukhin@ya.ru
+ */
+
+namespace App\Models;
+
+class Invite extends BaseModel
+{
+    protected $table = 'invites';
+}
