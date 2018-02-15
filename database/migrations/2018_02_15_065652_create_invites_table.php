7d26daa0fdbf875dd72f42a5520b10886bc41aaf
vleukhin <norad-2013@mail.ru>
2018-02-15 14:15:02 +0700

    таблица приглашений

---
+++ database/migrations/2018_02_15_065652_create_invites_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invites', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('uid')->unique();
+            $table->string('name');
+            $table->boolean('multiple')->default(false);
+            $table->boolean('accepted')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invites');
+    }
+}
