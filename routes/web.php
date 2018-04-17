a519f0c116dabe8f4a5ef890c6ac83a4ab96248e
vleukhin <norad-2013@mail.ru>
2018-04-17 18:21:44 +0700

    admin

--- routes/web.php
+++ routes/web.php
@@ -20,15 +20,3 @@ Route::as('login.form')->get('login', 'Auth\LoginController@showLoginForm');
 Route::as('logout')->get('logout', 'Auth\LoginController@logout');
 
 Route::as('invite')->get('invite/{invite_uid}', 'FrontController@showInvite');
-
-Route::middleware(['auth'])
-    ->prefix('admin')
-    ->namespace('Admin')
-    ->as('admin.')
-    ->group(function () {
-        Route::as('index')->get('/', 'IndexController@index');
-
-        Route::as('invites.')->group(function (){
-            Route::as('index')->get('/invites', 'InvitesController@index');
-        });
-    });
