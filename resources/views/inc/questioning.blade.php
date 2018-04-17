efac1b86e6487809c0e6bf598f943b5cba6ca2ee
vleukhin <norad-2013@mail.ru>
2018-04-17 20:23:57 +0700

    еще правки текстов

--- resources/views/inc/questioning.blade.php
+++ resources/views/inc/questioning.blade.php
@@ -4,40 +4,26 @@
         <form action="#">
             <div class="row inp-radio">
                 <div class="col-md-6 col-left clear">
-                    <label>Конечно, мы будем<input type="radio" name="group1"></label>
+                    <label>Конечно, {{ $invite->multiple ? 'мы будем' : 'я буду' }}<input type="radio" name="group1"></label>
                 </div>
                 <div class="col-md-6 col-right clear">
-                    <label>Мы не придем<input type="radio" name="group1"></label>
+                    <label>{{ $invite->multiple ? 'Мы не придем' : 'Я не приду' }}<input type="radio" name="group1"></label>
                 </div>
             </div>
-            <div class="info">Пожалуйста, чтобы все прошло идеально, ответьте еще на несколько вопросов</div>
+            <div class="info">Пожалуйста, чтобы все прошло идеально, {{ $invite->multiple ? 'ответьте' : 'ответь' }} еще на несколько вопросов</div>
             <div class="row radios">
-                <div class="col-md-4 car clear">
-                    <div class="name">Нужен ли Вам трансфер</div>
-                    <div class="quest-radio"><label><input type="radio" name="group2" class="radios-check">да</label></div>
-                    <div class="quest-radio"><label><input type="radio" name="group2" class="radios-check">нет</label></div>
-                </div>
                 <div class="col-md-4 food clear">
                     <div class="name">Какая еда предпочтительна</div>
                     <div class="quest-radio"><label><input type="radio" name="group3" class="radios-check">мясо</label></div>
                     <div class="quest-radio"><label><input type="radio" name="group3" class="radios-check">рыба</label></div>
                     <div class="quest-radio"><label><input type="radio" name="group3" class="radios-check">птица</label></div>
-                    <div class="quest-radio wide"><label><input type="radio" name="group3" class="radios-check">свой вариант</label></div>
-                    <div class="quest-text"><label><input type="text" name="group3-other" class="text-check"></label></div>
                 </div>
                 <div class="col-md-4 drink clear">
-                    <div class="name">Что будете пить</div>
+                    <div class="name">Что {{ $invite->multiple ? 'ответьте' : 'ответь' }} пить</div>
                     <div class="quest-radio"><label><input type="radio" name="group4" class="radios-check">вино</label></div>
                     <div class="quest-radio"><label><input type="radio" name="group4" class="radios-check">коньяк</label></div>
                     <div class="quest-radio"><label><input type="radio" name="group4" class="radios-check">виски</label></div>
-                    <div class="quest-radio"><label><input type="radio" name="group4" class="radios-check">другое</label></div>
-                    <div class="quest-text"><label><input type="text" name="group4-other" class="text-check"></label></div>
                 </div>
-            </div>
-            <div class="info">Если вы едете на своем авто, укажите количество свободных мест <input type="text" name="freecar"></div>
-            <div class="row submit">
-                <div class="col-md-6 col-left clear"><input type="text" name="contact" placeholder="телефон или почта"></div>
-                <div class="col-md-6 col-right clear"><input type="submit" value="Подтвердить"></div>
             </div>
         </form>
     </div>
